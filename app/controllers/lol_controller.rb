5ca54086222b4229207e1fbad35e127bf47cc67a
EC2 Default User <ec2-user@ip-172-31-45-93.us-east-2.compute.internal>
2018-11-20 14:56:44 +0000

    + rapport hebdo buggy à cause de l'api key j'espère

--- app/controllers/lol_controller.rb
+++ app/controllers/lol_controller.rb
@@ -2,17 +2,22 @@ class LolController < ApplicationController
     require 'net/http'
     require 'json'
     require 'date'
-    $API_KEY = "RGAPI-678a5a41-14b8-40b3-9057-554f14adec98"
+    $API_KEY = "RGAPI-cb510d6f-0a93-461d-9c15-20c5dfff9638"
     
     def index # Liste les derniers matchs du joueur
         uri = URI("https://euw1.api.riotgames.com/lol/match/v3/matchlists/by-account/32487406?api_key=#{$API_KEY}")
         
         
-        @matchs = JSON.parse(Net::HTTP.get(uri))["matches"].first(25) # 25 derniers matchs
+        @matchs = JSON.parse(Net::HTTP.get(uri))["matches"]
         
-        @choisis = select_matchs(@matchs)
+        @mhier = select_hier(@matchs) #Matchs d'hier
+        @mlastweek = select_lastweek(@matchs) #Matchs de la semaine précédente
         
-        @temps_hier = add_durations(@choisis)
+        @total_hier_secondes = add_durations(@mhier)
+        @hier_time = Time.at(add_durations(@mhier)).utc.strftime("%H heures, %M minutes, et %S secondes")
+        
+        @total_lastweek_secondes = add_durations(@mlastweek)
+        @lastweek_time = Time.at(add_durations(@mlastweek)).utc.strftime("%H heures, %M minutes, et %S secondes")
     end
     
     def show # Reste plus qu'à sélectionner le dernier timestamp, puis convertir en minutes
@@ -22,8 +27,8 @@ class LolController < ApplicationController
     end
     
     private
-    def select_matchs(liste)
-        
+    def select_hier(liste)
+        return nil if liste == nil
         selected_matchs = []
         liste.each {|i|
             hier = (DateTime.yesterday).strftime("%m/%d")
@@ -36,15 +41,35 @@ class LolController < ApplicationController
         return selected_matchs
     end
     
+    def select_lastweek(liste)
+        return nil if liste == nil
+        
+        selected_matchs = []
+        liste.each {|i|
+            now = DateTime.now
+            lastw = now-now.wday-6
+            thisw = now-now.wday
+            
+            debut_sem = lastw.strftime("%m/%d")
+            fin_sem = thisw.strftime("%m/%d")
+            jour_match = DateTime.strptime("#{i["timestamp"]}".first(10), '%s').strftime("%m/%d")
+            
+            if jour_match <= fin_sem && jour_match >= debut_sem
+                selected_matchs << i["gameId"]
+            end
+        }
+        return selected_matchs
+    end
+    
     def add_durations(match_ids)
+        return nil if match_ids == nil
         total_seconds = 0
         match_ids.each {|i|
-            uri = URI("https://euw1.api.riotgames.com//lol/match/v3/matches/3831204745?api_key=#{$API_KEY}")
+            uri = URI("https://euw1.api.riotgames.com//lol/match/v3/matches/#{i}?api_key=#{$API_KEY}")
         
             match_infos = JSON.parse(Net::HTTP.get(uri))
-            
-            total_seconds += match_infos["gameDuration"]
+            total_seconds += match_infos["gameDuration"] if match_infos["gameDuration"] != nil
         }
-        Time.at(total_seconds).utc.strftime("%H:%M:%S")
+        return total_seconds
     end
 end
